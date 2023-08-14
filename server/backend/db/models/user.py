bb00208c4fe4d7eae5b67269f0a72e1c3a5a2ab4
Rostyslav Malaniuk <rostyslav.malaniuk@gmail.com>
2023-08-15 00:06:24 +0300

    Add models and migrations

---
+++ server/backend/db/models/user.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, DateTime, Index, Integer, String, func
+
+from ..base import Base
+from .constants import MAX_LENGTH_USER_NAME
+
+
+class User(Base):
+    id = Column(Integer, primary_key=True, autoincrement=True, comment="Unique id")
+    username = Column(
+        String(MAX_LENGTH_USER_NAME), nullable=False, comment="Unique email address"
+    )
+    hashed_password = Column(String, nullable=False, comment="Hashed password")
+    created_at = Column(
+        DateTime(timezone=False),
+        default=func.now(),
+        server_default=func.now(),
+        comment="User created at",
+    )
+
+    _table_args_ = (
+        Index("ix_user_id_btree", id, postgresql_using="btree"),
+        Index(
+            "ix_user_username_btree", username, unique=True, postgresql_using="btree"
+        ),
+        Index("ix_user_created_at_btree", created_at, postgresql_using="btree"),
+    )
---
+++ server/backend/db/session.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from ..service.core.settings import settings
+
+engine = create_engine(
+    settings.SQLALCHEMY_DATABASE_URI,
+    pool_pre_ping=True,
+    echo=False,
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
